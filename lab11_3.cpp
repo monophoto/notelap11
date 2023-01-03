f3cf10b86676186e31f7b6906e98d4ac2916d012
monophto <jetsadawut1013@gmail.com>
2023-01-03 15:58:08 +0700

    complete_lab3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,32 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int data =0;
+    double Mean, deviation, sum, sum1;
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        sum1 += pow(atof(textline.c_str()),2);
+        data++;
+    }
+
+    Mean = sum/data;
+    deviation = pow(((sum1/data)-(pow(Mean,2))),0.5);
+
+    cout << "Number of data = "<< data << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << Mean << '\n';
+    cout << "Standard deviation = " << deviation;
+}
